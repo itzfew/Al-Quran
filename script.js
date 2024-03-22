d929d26c68f6b387b2281c311a459340d2955245
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-03-22 19:02:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('surah-yaseen').innerHTML = text;
     })
     .catch(error => console.log('Error fetching Surah Yaseen text:', error));
+
+    // Add event listener to Surah Yaseen heading for toggle effect
+    document.getElementById('surah-yaseen-heading').addEventListener('click', function() {
+        var panel = document.getElementById('surah-yaseen-panel');
+        if (panel.style.display === 'block') {
+            panel.style.display = 'none';
+        } else {
+            panel.style.display = 'block';
+        }
+    });
 });
